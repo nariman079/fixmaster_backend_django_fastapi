436d86241e4111e0fd676c321c1d2757cbc76f13
nariman079 <nariman079i@mail.ru>
2024-05-19 11:14:33 +0300

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'localhost',
         'HOST': 'db',
         'PORT': '5433',
     }
