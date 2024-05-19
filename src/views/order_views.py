f88fbabb8189111ae5eab3ee310761ed4b6f8a8b
nariman079 <nariman079i@mail.ru>
2024-05-19 12:15:56 +0300

    fix errors

--- src/views/order_views.py
+++ src/views/order_views.py
@@ -1,4 +1,4 @@
-from typing import OrderedDict
+from collections import OrderedDict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.request import Request
