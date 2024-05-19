26443fa4f30880b80eec7d6e1f5df4bdb0b8bebc
nariman079 <nariman079i@mail.ru>
2024-05-19 12:08:16 +0300

    added drf_spectacular

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework',
     "corsheaders",
     'drf_spectacular',
+    'drf_spectacular_sidecar',
     *ROOT_APPS
 ]
 MIDDLEWARE = [
@@ -69,9 +70,14 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
-    }
-}
+    },
 
+}
+if DEBUG:
+    DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
