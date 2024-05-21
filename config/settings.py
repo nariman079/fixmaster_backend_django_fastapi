7fddacaf9969e809bd25448111e2fd62208bd929
nariman079 <nariman079i@mail.ru>
2024-05-21 08:25:11 +0300

    fix db settings

--- config/settings.py
+++ config/settings.py
@@ -65,9 +65,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     },
