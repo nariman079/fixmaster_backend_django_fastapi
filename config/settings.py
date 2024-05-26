cbbcdb2c1af749c2c241cffdc940d2c720b900dc
nariman079 <nariman079i@mail.ru>
2024-05-26 09:41:31 +0300

    free times on bookings depends on start_time and end_time organization

--- config/settings.py
+++ config/settings.py
@@ -72,10 +72,10 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
     },
 }
-DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
