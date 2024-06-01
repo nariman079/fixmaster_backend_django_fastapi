c941b0553ec56a0dcf4d39b5c7a8a7d89200c282
nariman079 <nariman079i@mail.ru>
2024-06-01 13:50:57 +0300

    fix nulled images

--- src/services/image_services.py
+++ src/services/image_services.py
@@ -8,4 +8,6 @@ def get_full_url(serializer, model, image_field: str) -> str:
     """
     request = serializer.context.get('request')
     main_image_url = getattr(model, image_field).url if getattr(model, image_field) else ''
+    if main_image_url == '':
+        return getattr(model, image_field+"_url")
     return request.build_absolute_uri(main_image_url)
