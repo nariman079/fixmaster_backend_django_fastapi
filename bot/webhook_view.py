a29a675e280a067fccd39f82234fa7b3800028cc
nariman079 <nariman079i@mail.ru>
2024-06-07 16:55:50 +0300

    fix webhook

--- bot/webhook_view.py
+++ bot/webhook_view.py
@@ -1,4 +1,3 @@
-import flask
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 import telebot
@@ -6,7 +5,6 @@ import telebot
 
 from bot.config import client_bot
 
-flask.request.get_data()
 def webhook_organization(request: HttpRequest):
     json_string = request.body.decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
