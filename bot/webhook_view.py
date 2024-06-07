58ebda83f1650bc8884e2fd39189210002a5995d
nariman079 <nariman079i@mail.ru>
2024-06-07 16:50:58 +0300

    added telegram webhook

--- bot/webhook_view.py
+++ bot/webhook_view.py
@@ -7,7 +7,7 @@ from bot.config import client_bot
 
 
 def webhook_organization(request: HttpRequest):
-    json_string = request.POST.decode('utf-8')
+    json_string = request.POST
     update = telebot.types.Update.de_json(json_string)
     client_bot.process_new_updates([update])
     return HttpResponse(status=200)
