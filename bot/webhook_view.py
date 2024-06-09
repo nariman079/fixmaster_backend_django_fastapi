b1d3b9fc765e63fd79983fd998813c564c83e886
nariman079 <nariman079i@mail.ru>
2024-06-10 00:00:07 +0300

    off client webhook

--- bot/webhook_view.py
+++ bot/webhook_view.py
@@ -3,10 +3,3 @@ from django.http.response import HttpResponse
 import telebot
 
 
-from bot.config import client_bot
-
-def webhook_organization(request: HttpRequest):
-    json_string = request.body.decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    client_bot.process_new_updates([update])
-    return HttpResponse(status=200)
