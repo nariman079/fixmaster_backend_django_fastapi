3bb4bafa5ee1335af298c85ee94a8640f0e6c16d
nariman079 <nariman079i@mail.ru>
2025-09-22 09:39:42 +0300

    fix errors

--- app/nofication_service/src/interfaces.py
+++
@@ -1,7 +0,0 @@
-from abc import abstractmethod, ABC
-
-
-class BotRepository(ABC):
-    @abstractmethod
-    async def send(self, chat_id: int, text: str, reply_markup=None) -> None:
-        pass
--- app/nofication_service/src/messages/telegram/__init__.py
+++
@@ -1,6 +0,0 @@
-from .client import ClientMessages
-from .master import MasterMessages
-from .moderatior import ModeratorMessages
-from .organization import OrganizationMessages
-
-__all__ = (ClientMessages, MasterMessages, ModeratorMessages, OrganizationMessages)
